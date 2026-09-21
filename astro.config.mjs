09c6c43c316073902b74ba3a6f437518740cf281
HiddenInNet <dgongar3112@gmail.com>
2026-09-22 00:18:38 +0200

    feat: crea proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  site: 'https://etsii.es',
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  adapter: cloudflare(),
 });
