5ee6bb4f392d561bfa3c98154fced756194cbe18
HiddenInNet <dgongar3112@gmail.com>
2026-09-22 16:26:56 +0200

    feat: añade contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import sitemap from '@astrojs/sitemap';
-import cloudflare from "@astrojs/cloudflare";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import cloudflare from '@astrojs/cloudflare'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://etsii.es',
+  output: 'server',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
   adapter: cloudflare(),
-});
+})
