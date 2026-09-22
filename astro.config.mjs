b18754276339d3ab247fee1400e8536353651f74
HiddenInNet <dgongar3112@gmail.com>
2026-09-22 17:39:44 +0200

    fix: arregla el fichero de configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://etsii.es',
+  site: 'https://www.etsii.es',
   output: 'server',
   integrations: [sitemap()],
   vite: {
